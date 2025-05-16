0436d5c7299bbc14ef8f0c0adb694e0407240959
Strato <benjamin@thiebaut.me>
2025-05-16 14:43:46 +0200

    Move app start to root

--- app.ts
+++ app.ts
@@ -7,12 +7,12 @@ import StoreFactory from 'better-sqlite3-session-store';
 import JwtStrategy from 'passport-jwt';
 import path from 'path';
 import Database from 'better-sqlite3';
-import Utils from "./Utils/Utils";
-import Server from "./Server";
-import LoginController from "./Controller/LoginController";
-import MailService from "./Services/MailService";
-import HomeController from "./Controller/HomeController";
-import { initPassport } from "./Security/passport.mw"
+import Utils from "./src/Utils/Utils";
+import Server from "./src/Server";
+import LoginController from "./src/Controller/LoginController";
+import MailService from "./src/Services/MailService";
+import HomeController from "./src/Controller/HomeController";
+import { initPassport } from "./src/Security/passport.mw"
 
 const SqliteStore = StoreFactory(session);
 
